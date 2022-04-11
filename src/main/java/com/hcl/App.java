ff239d7785840ba29a18397fd4d7072e5b84fbaf
AACoderBeg <abrarahmad.cs@gmail.com>
2022-04-11 10:55:20 -0500

    My second commit to maven

--- src/main/java/com/hcl/App.java
+++ src/main/java/com/hcl/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is Abrar!" );
+        System.out.println("This is my first maven project!");
     }
 }
